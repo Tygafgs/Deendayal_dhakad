47f96b305dee80444ea78c9899b1bb7f358f8a59
molly4252 <mollydeanx1@gmail.com>
2025-04-26 15:36:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ MULTI_FSUB = [int(channel_id) for channel_id in environ.get('MULTI_FSUB', '').sp
 # Payment Configuration
 # ============================
 QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/e419f801841c2ee3db0fc.jpg')
-OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'deendayaldhaked@ybl')
+OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'https://t.me/+EZbSiNYRRo0wNTI1')
 
 # ============================
 # MongoDB Configuration
